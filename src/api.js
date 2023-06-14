16ebe006cf16911f44b92aa8b806efcf5d34c0c1
StasLiubinetskyi <121062371+StasLiubinetskyi@users.noreply.github.com>
2023-06-14 17:32:00 +0300

    axios

--- src/api.js
+++ src/api.js
@@ -1,9 +1,16 @@
+import axios from 'axios';
+
 const API_KEY = '37206496-4ba23d7a61facc457fce3b97c';
 const BASE_URL = `https://pixabay.com/api/?key=${API_KEY}&image_type=photo&orientation=horizontal&safesearch=true`;
 
 export async function searchImages(searchQuery, page) {
   const apiUrl = `${BASE_URL}&q=${searchQuery}&page=${page}&per_page=40`;
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data.hits;
+
+  try {
+    const response = await axios.get(apiUrl);
+    const data = response.data.hits;
+    return data;
+  } catch (error) {
+    throw new Error('An error occurred while fetching images.');
+  }
 }
